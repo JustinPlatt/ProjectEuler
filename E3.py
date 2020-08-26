946e35bbcb1b4189889e8f38e3268e533262f943
Justin Platt <justinplatt08@gmail.com>
2020-08-26 13:17:31 -0500

    P3 initial upload

---
+++ E3.py
@@ -0,0 +1,28 @@
+#The prime factors of 13195 are 5, 7, 13 and 29.
+
+#What is the largest prime factor of the number 600851475143 ?
+
+def is_prime(x):
+    result = True
+    for a in range(2, x): #loop from 2 through x-1
+        if x % a == 0:
+            result = False
+    return result
+
+
+def main():
+    cap = int(input('Factor which number?:  '))  #put the number to factor here
+    print('Printing factors of ' + str(cap))
+
+    #we want to generate a list of primes.
+    #If we hit a prime, divide the existing number until there's a remainder
+    #then change the cap to be whatever's left after the division
+    cur_num = 2
+    while cur_num <= cap:
+        while cap % cur_num == 0:
+            print(cur_num)
+            cap = cap / cur_num
+        cur_num += 1
+
+if __name__ == "__main__":
+    main()
