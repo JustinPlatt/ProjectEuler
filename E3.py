6fe24ba2d27aa3cae4e8a8086b538b0dea2366ca
Justin Platt <justinplatt08@gmail.com>
2020-08-26 13:24:56 -0500

    P3 removed unneeded is_prime function

--- E3.py
+++ E3.py
@@ -1,13 +1,15 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
-
 #What is the largest prime factor of the number 600851475143 ?
 
-def is_prime(x):
-    result = True
-    for a in range(2, x): #loop from 2 through x-1
-        if x % a == 0:
-            result = False
-    return result
+
+###No function is needed to test for primes because only
+###primes will divide evenly into cap
+#def is_prime(x):
+#    result = True
+#    for a in range(2, x): #loop from 2 through x-1
+#        if x % a == 0:
+#            result = False
+#    return result
 
 
 def main():
